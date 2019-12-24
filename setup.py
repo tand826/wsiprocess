bfe1ebf698ad532dab95c557c0a32e5e56d2291f
takumiando <takumiando@m.u-tokyo.ac.jp>
2019-12-24 14:06:33 +0900

    update setuppy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wsiprocess",  # Replace with your own username
+    name="wsiprocess",
     version="0.0.1",
     author="Takumi Ando",
     author_email="takumi.ando826@gmail.com",
