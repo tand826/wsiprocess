b4f70c56797562d20cfb75be79b4ceba1d2cb2fb
tand826 <takumi.ando826@gmail.com>
2020-04-15 13:59:29 +0900

    add comments

--- wsiprocess/slide.py
+++ wsiprocess/slide.py
@@ -1,9 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Slide object to pass to annotation object and patcher object.
+Slide is whole slide image, scanned with whole slide scanners.
+Mannually you can make pyramidical tiff file, which you can handle just the
+same as the scanned digital data, except for the magnification.
+"""
+
 import pyvips
 from pathlib import Path
 from .error import SlideLoadError
 
 
 class Slide:
+    """Slide object.
+
+    Args:
+        path (str): Path to the whole slide image file.
+
+    Attributes:
+        path (str): Path to the whole slide image file.
+        slide (pyvips.Image): pyvips Image object.
+        wsi_width (int): Width of slide.
+        wsi_height (int): Height of slide.
+    """
 
     def __init__(self, path):
         self.path = path
@@ -17,13 +35,29 @@ class Slide:
         self.set_properties()
 
     def export_thumbnail(self, save_to=".", size=500):
+        """Export thumbnail image.
+
+        Args:
+            save_to (str): Parent directory to save the thumbnail image.
+            size (int, optional): Size of the exported thumbnail.
+        """
         thumb = self.get_thumbnail(size)
         thumb.pngsave("{}/thumb.png".format(save_to))
 
     def get_thumbnail(self, size=500):
+        """Get thumbnail image.
+
+        Args:
+            size (int, optional): Size of the exported thumbnail.
+        """
         return pyvips.Image.thumbnail(self.path, size)
 
     def set_properties(self):
+        """Read the properties and set as attributes of slide obj.
+
+        Attributes:
+            magnification (int): Objective power of slide obj.
+        """
         for field in self.slide.get_fields():
             setattr(self, field, self.slide.get(field))
         if "openslide.objective-power" in self.slide.get_fields():
