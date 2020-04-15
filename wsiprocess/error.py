c93b6063df273d1e6f6718f60896a9a91e5be1ee
tand826 <takumi.ando826@gmail.com>
2020-04-15 13:41:52 +0900

    added comments

--- wsiprocess/error.py
+++ wsiprocess/error.py
@@ -1,23 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Custom errors for wsiprocess.
+"""
+
+
 class WsiProcessError(Exception):
+    """Root error class.
+
+    Args:
+        message (str): Message to show in the stdout.
+    """
     def __init__(self, message):
         self.message = message
 
 
 class SlideLoadError(WsiProcessError):
+    """Error on loading slides.
+
+    Args:
+        message (str): Message to show in the stdout.
+    """
     def __init__(self, message):
         super().__init__(message)
 
 
 class MissCombinationError(WsiProcessError):
+    """Error of the combination of the method and the anntoation file.
+
+    Args
+        message (str): Message to show in the stdout.
+    """
     def __init__(self, message):
         super().__init__(message)
 
 
 class PatchSizeTooSmallError(WsiProcessError):
+    """Error of the size of patches.
+
+    This should be warning?
+
+    Args:
+        message (str): Message to show in the stdout.
+    """
     def __init__(self, message):
         super().__init__(message)
 
 
 class SizeError(WsiProcessError):
+    """Error of sizes.
+
+    The slide size is larger than the patches, and the patch size is larger
+    than the overlap size.
+
+    Args:
+        message (str): Message to show in the stdout.
+    """
     def __init__(self, message):
         super().__init__(message)
