c2211d2f10a8c38915a3ad243d403213a247f945
tand826 <takumi.ando826@gmail.com>
2021-05-26 23:16:41 +0900

    update converter to coco

--- wsiprocess/converters/wsiprocess_to_coco.py
+++ wsiprocess/converters/wsiprocess_to_coco.py
@@ -7,13 +7,29 @@ Only available for "detection" method, but works when extracting patches and
 after extraction.
 
 """
-import sys
 import argparse
 import shutil
 import random
 import json
 from datetime import datetime
 from pathlib import Path
+
+import cv2
+
+
+class SaveTo(type(Path())):
+
+    def __init__(self, path):
+        self.annotations = self/"annotations"
+        self.train = self/"train2014"
+        self.val = self/"val2014"
+        self.test = self/"test2014"
+
+        self.mkdir(exist_ok=True)
+        self.annotations.mkdir(exist_ok=True)
+        self.train.mkdir(exist_ok=True)
+        self.val.mkdir(exist_ok=True)
+        self.test.mkdir(exist_ok=True)
 
 
 class ToCOCOConverter:
@@ -22,19 +38,44 @@ class ToCOCOConverter:
 
     def __init__(self, params=False):
         if params:
-            self.root = params["root"]
-            self.save_to = params["save_to"]/"coco"
+            self.root = Path(params["root"])
+            self.save_to = SaveTo(f"{params['save_to']}/coco")
             self.ratio_arg = params["ratio_arg"]
         else:
             self.getargs()
+        self.test_is_available = len(self.ratio_arg.split(":")) == 3
+        self.phases = ["train", "val"]
+        if self.test_is_available:
+            self.phases.append("test")
+
+        self.now = datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
+        self.paths = {phase: [] for phase in self.phases}
+        self.get_base()
         self.get_ratio()
-        self.make_dirs()
+        self.read_annotation()
+        self.set_id()
+
+    def set_id(self):
+        if self.test_is_available:
+            target = "instances_val2014.json"
+        else:
+            target = "instances_test2014.json"
+        if (self.save_to.annotations/target).exists():
+            with open(self.save_to.annotations/target, "r") as f:
+                data = json.load(f)
+                last_image_id = data["images"][-1]["id"]
+                last_annotation_id = data["annotations"][-1]["id"]
+        else:
+            last_image_id, last_annotation_id = 0, 0
+        self.image_id = last_image_id + 1
+        self.annotation_id = last_annotation_id + 1
 
     def convert(self):
         self.make_link_to_images()
-        self.get_save_as()
-        self.annotations_to_json()
-        self.make_output()
+        self.add_info()
+        self.add_categories()
+        self.add_images()
+        self.add_annotations()
         self.save_data()
 
     def getargs(self):
@@ -50,279 +91,192 @@ class ToCOCOConverter:
         self.save_to = args.save_to
         self.ratio_arg = args.ratio
 
-    def make_dirs(self):
-        if not self.save_to.exists():
-            self.save_to.mkdir(parents=True)
-
-        if not (self.save_to/"annotations").exists():
-            (self.save_to/"annotations").mkdir()
-
-        if not (self.save_to/"train2014").exists():
-            (self.save_to/"train2014").mkdir()
-
-        if not (self.save_to/"val2014").exists():
-            (self.save_to/"val2014").mkdir()
-
-        if self.test_is_available:
-            if not (self.save_to/"test2014").exists():
-                (self.save_to/"test2014").mkdir()
-
     def get_ratio(self):
-        self.test_is_available = len(self.ratio_arg.split(":")) == 3
-        if self.test_is_available:
-            train, val, test = map(int, self.ratio_arg.split(":"))
-            self.ratio = {
-                "train": train/(train+val+test),
-                "val": val/(train+val+test),
-                "test": test/(train+val+test)}
-        else:
-            train, val = map(int, self.ratio_arg.split(":"))
-            self.ratio = {"train": train/(train+val), "val": val/(train+val)}
+        ratios = list(map(float, self.ratio_arg.split(":")))
+        self.ratio = {p: r / sum(ratios) for p, r in zip(self.phases, ratios)}
+
+    def add_info(self):
+        for phase in self.phases:
+            self.coco_data[phase]["info"] = info(self.now)
 
     def make_link_to_images(self):
-        classes = [i.stem for i in (self.root/"patches").glob("*")]
-        self.train_paths = []
-        self.val_paths = []
-        if self.test_is_available:
-            self.test_paths = []
-        for cls in classes:
-            image_paths = list((self.root/"patches").glob(f"{cls}/*"))
-            random.shuffle(image_paths)
+        for cls in self.annotation["classes"]:
+            paths = list((self.root/"patches").glob(f"{cls}/*"))
+            random.shuffle(paths)
 
-            train_count = int(len(image_paths)*self.ratio["train"])
-            self.train_paths += image_paths[:train_count]
+            train_count = int(len(paths)*self.ratio["train"])
+            self.paths["train"] += paths[:train_count]
             if self.test_is_available:
-                val_count = int(len(image_paths)*self.ratio["val"])
+                val_count = int(len(paths)*self.ratio["val"])
                 val_count_end = train_count + val_count
-                self.val_paths += image_paths[train_count:val_count_end]
-                self.test_paths += image_paths[train_count+val_count:]
+                self.paths["val"] += paths[train_count:val_count_end]
+                self.paths["test"] += paths[train_count+val_count:]
             else:
-                self.val_paths += image_paths[train_count:]
+                self.paths["val"] += paths[train_count:]
 
-            self._make_link_to_images_per_phase("train", cls)
-            self._make_link_to_images_per_phase("val", cls)
-            if self.test_is_available:
-                self._make_link_to_images_per_phase("test", cls)
+        for phase in self.phases:
+            for image_path in self.paths[phase]:
+                img_name = image_path.name
+                if not (self.save_to/f"{phase}2014"/img_name).exists():
+                    shutil.copy(image_path, self.save_to/f"{phase}2014")
 
-    def _make_link_to_images_per_phase(self, phase, cls):
-        if phase == "train":
-            paths = self.train_paths
-        elif phase == "val":
-            paths = self.val_paths
-        elif phase == "test":
-            paths = self.test_paths
-        for image_path in paths:
-            img_name = image_path.name
-            if not (self.save_to/f"{phase}2014"/img_name).exists():
-                shutil.copy(image_path, self.save_to/f"{phase}2014")
+    def get_base(self):
+        self.coco_data = {}
+        for phase in self.phases:
+            save_to = self.save_to.annotations
+            json_path = save_to/f"instances_{phase}2014.json"
+            if json_path.exists():
+                with open(json_path, "r") as f:
+                    self.coco_data[phase] = json.load(f)
+            else:
+                self.coco_data[phase] = base()
 
-    def get_save_as(self):
-        now = datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
-        train_path = self.save_to/"annotations"/"instances_train2014.json"
-        if not train_path.exists():
-            self.train2014 = {
-                "info": {
-                    "description": "wsiprocess",
-                    "url": "",
-                    "version": "1.0",
-                    "year": 2014,
-                    "contributor": "",
-                    "date_created": now
-                },
-                "licenses": [],
-                "categories": [],
-                "images": [],
-                "annotations": []
-            }
-
-        else:
-            with open(train_path, "r") as f:
-                self.train2014 = json.load(f)
-
-        val_path = self.save_to/"annotations"/"instances_val2014.json"
-        if not val_path.exists():
-            self.val2014 = {
-                "info": {
-                    "description": "wsiprocess",
-                    "url": "",
-                    "version": "1.0",
-                    "year": "2014",
-                    "contributor": "",
-                    "date_created": now
-                },
-                "licenses": [],
-                "categories": [],
-                "images": [],
-                "annotations": []
-            }
-        else:
-            with open(val_path, "r") as f:
-                self.val2014 = json.load(f)
-
-        test_path = self.save_to/"annotations"/"instances_test2014.json"
-        if not test_path.exists():
-            self.test2014 = {
-                "info": {
-                    "description": "wsiprocess",
-                    "url": "",
-                    "version": "1.0",
-                    "year": "2014",
-                    "contributor": "",
-                    "date_created": now
-                },
-                "licenses": [],
-                "categories": [],
-                "images": [],
-                "annotations": []
-            }
-        else:
-            with open(test_path, "r") as f:
-                self.test2014 = json.load(f)
-
-    def annotations_to_json(self):
+    def read_annotation(self):
         with open(self.root/"results.json", "r") as f:
             self.annotation = json.load(f)
 
-    def make_output(self):
-        last_image_id, last_annotation_id = self.read_last_data()
-        image_id = last_image_id + 1
-        annotation_id = last_annotation_id + 1
+    def add_categories(self):
+        for idx, cls in enumerate(self.annotation["classes"]):
+            for phase in self.phases:
+                self.add_category(phase, idx, cls)
 
-        classes = self.annotation["classes"]
-        slidestem = Path(self.annotation["slide"]).stem
-        patch_width = self.annotation["patch_width"]
-        patch_height = self.annotation["patch_height"]
+    def add_images(self):
+        for phase in self.phases:
+            for path in self.paths[phase]:
+                self.add_image(phase, path)
 
-        for cls in classes:
-            self.train2014 = self.add_categories(self.train2014, cls)
-            self.val2014 = self.add_categories(self.val2014, cls)
-            if self.test_is_available:
-                self.test2014 = self.add_categories(self.test2014, cls)
+    def add_annotations(self):
+        for phase in self.phases:
+            for path in self.paths[phase]:
+                x, y = map(int, path.stem.split("_")[-2:])
+                self.add_annotation(phase, x, y)
 
-        for idx, train_path in enumerate(self.train_paths):
-            x, y = map(int, train_path.stem.split("_")[-2:])
-            image_params, image_id = self.get_image_params(
-                train_path, slidestem, x, y, patch_width, patch_height,
-                image_id)
-            self.train2014["images"].append(image_params)
+    def add_category(self, phase, category_idx, name):
+        self.coco_data[phase]["categories"].append(category(
+            category_idx, name)
+        )
 
-            annotation_params, annotation_id = self.get_annotation_params(
-                self.annotation, classes, train_path, slidestem, x, y,
-                patch_width, patch_width, image_id, annotation_id)
-            self.train2014["annotations"].extend(annotation_params)
+    def add_image(self, phase, file_name):
+        self.coco_data[phase]["images"].append(
+            image(
+                file_name, self.annotation["patch_width"],
+                self.annotation["patch_height"], self.image_id)
+        )
+        self.image_id += 1
 
-            image_id += 1
-
-        for idx, val_path in enumerate(self.val_paths):
-            x, y = map(int, val_path.stem.split("_")[-2:])
-            image_params, image_id = self.get_image_params(
-                val_path, slidestem, x, y, patch_width, patch_height, image_id)
-            self.val2014["images"].append(image_params)
-
-            annotation_params, annotation_id = self.get_annotation_params(
-                self.annotation, classes, val_path, slidestem, x, y,
-                patch_width, patch_width, image_id, annotation_id)
-            self.val2014["annotations"].extend(annotation_params)
-
-            image_id += 1
-
-        if self.test_is_available:
-            for idx, test_path in enumerate(self.test_paths):
-                x, y = map(int, test_path.stem.split("_")[-2:])
-                image_params, image_id = self.get_image_params(
-                    test_path, slidestem, x, y, patch_width, patch_height,
-                    image_id)
-                self.test2014["images"].append(image_params)
-
-                annotation_params, annotation_id = self.get_annotation_params(
-                    self.annotation, classes, test_path, slidestem, x, y,
-                    patch_width, patch_width, image_id, annotation_id)
-                self.test2014["annotations"].extend(annotation_params)
-
-                image_id += 1
-
-    def read_last_data(self):
-        if self.test_is_available:
-            target = "instances_val2014.json"
-        else:
-            target = "instances_test2014.json"
-        if (self.save_to/"annotations"/target).exists():
-            with open(self.save_to/"annotations"/target, "r") as f:
-                data = json.load(f)
-                last_image_id = data["images"][-1]["id"]
-                last_annotation_id = data["annotations"][-1]["id"]
-        else:
-            last_image_id, last_annotation_id = 0, 0
-
-        return last_image_id, last_annotation_id
-
-    def add_categories(self, annotation, cls):
-        categories = annotation["categories"]
-        classes = [cat["name"] for cat in categories]
-        if cls not in classes:
-            categories.append({
-                "supercategory": "",
-                "id": len(classes)+1,
-                "name": cls}
-            )
-        return annotation
-
-    def get_image_params(
-            self, file_name, slidestem, x, y, width, height, image_id):
-        return {
-            "license": "",
-            "file_name": str(file_name),
-            "coco_url": "",
-            "width": width,
-            "height": height,
-            "date_captured": "",
-            "flickr_url": "",
-            "id": image_id
-        }, image_id
-
-    def get_annotation_params(
-            self, annotation, classes, file_name, slidestem, x, y, width,
-            height, image_id, annotation_id):
-        annotations = []
-        for box in annotation["result"]:
-            if box["x"] == x and box["y"] == y:
-                if box.get("bbs"):
-                    for bb in box["bbs"]:
-                        bb["x"] %= width
-                        bb["y"] %= height
-                        data = {
-                            "segmentation": [[
-                                bb["x"], bb["y"],
-                                bb["x"] + bb["w"], bb["y"],
-                                bb["x"] + bb["w"], bb["y"] + bb["h"],
-                                bb["x"], bb["y"] + bb["h"]
-                            ]],
-                            "area": bb["w"]*bb["h"],
-                            "iscrowd": 0,
-                            "image_id": image_id,
-                            "bbox": [bb["x"], bb["y"], bb["w"], bb["h"]],
-                            "category_id": classes.index(bb["class"]) + 1,
-                            "id": annotation_id
-                        }
-                        annotation_id += 1
-                        annotations.append(data)
-                else:
-                    print(f"{file_name} have no bounding boxes.")
-                    sys.exit()
-        return annotations, annotation_id
+    def add_annotation(self, phase, x, y):
+        for result in self.annotation["result"]:
+            if result["x"] != x or result["y"] != y:
+                continue
+            offsetx = x
+            offsety = y
+            if result.get("bbs"):
+                for bb in result["bbs"]:
+                    bb["x"] %= self.annotation["patch_width"]
+                    bb["y"] %= self.annotation["patch_height"]
+                    self.coco_data[phase]["annotations"].append(
+                        annotation(
+                            bb, offsetx, offsety, self.image_id,
+                            self.annotation_id, self.annotation["classes"],
+                            mode="detection")
+                    )
+                    self.annotation_id += 1
+            elif result.get("masks"):
+                for mask in result["masks"]:
+                    annotations = annotation(
+                        mask, offsetx, offsety, self.image_id,
+                        self.annotation_id, self.annotation["classes"],
+                        mode="segmentation")
+                    self.coco_data[phase]["annotations"].extend(annotations)
+                    self.annotation_id += len(annotations)
 
     def save_data(self):
-        annotation_dir = self.save_to/"annotations"
+        for phase in self.phases:
+            target = f"instances_{phase}2014.json"
+            with open(self.save_to.annotations/target, "w") as f:
+                json.dump(self.coco_data[phase], f, indent=4)
 
-        with open(annotation_dir/"instances_train2014.json", "w") as f:
-            json.dump(self.train2014, f, indent=4)
 
-        with open(annotation_dir/"instances_val2014.json", "w") as f:
-            json.dump(self.val2014, f, indent=4)
+def base():
+    return {
+        "info": {},
+        "licenses": [],
+        "categories": [],
+        "images": [],
+        "annotations": []
+    }
 
-        if self.test_is_available:
-            with open(annotation_dir/"instances_test2014.json", "w") as f:
-                json.dump(self.test2014, f, indent=4)
+
+def info(now):
+    return {
+        "description": "wsiprocess",
+        "url": "",
+        "version": "1.0",
+        "year": 2014,
+        "contributor": "",
+        "date_created": now
+    }
+
+
+def category(category_idx, name):
+    # category_idx starts from 0 -> plus 1 to set as id
+    return {
+        "supercategory": "",
+        "id": category_idx+1,
+        "name": name}
+
+
+def image(file_name, width, height, image_id):
+    return {
+        "license": "",
+        "file_name": str(file_name),
+        "coco_url": "",
+        "width": width,
+        "height": height,
+        "date_captured": "",
+        "flickr_url": "",
+        "id": image_id
+    }
+
+
+def annotation(
+        result, offsetx, offsety, image_id, annotation_id, classes, mode):
+    if mode == "detection":
+        bb = result
+        return {
+            "segmentation": [[
+                bb["x"], bb["y"],
+                bb["x"] + bb["w"], bb["y"],
+                bb["x"] + bb["w"], bb["y"] + bb["h"],
+                bb["x"], bb["y"] + bb["h"]
+            ]],
+            "area": bb["w"]*bb["h"],
+            "iscrowd": 0,
+            "image_id": image_id,
+            "bbox": [bb["x"], bb["y"], bb["w"], bb["h"]],
+            "category_id": classes.index(bb["class"]) + 1,
+            "id": annotation_id
+        }
+    elif mode == "segmentation":
+        mask = result
+        mask_img = cv2.imread(mask["coords"], 0)
+        contours, _ = cv2.findContours(
+            mask_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        annotations = []
+        for contour in contours:
+            contour = contour.reshape(-1, 2) + [offsetx, offsety]
+            minx, miny = list(map(int, contour.min(axis=0)))
+            maxx, maxy = list(map(int, contour.max(axis=0)))
+            annotations.append({
+                "segmentation": contour.ravel().tolist(),
+                "area": int((mask_img == 1).sum()),
+                "iscrowd": 0,
+                "image_id": image_id,
+                "bbox": [minx, miny, maxx - minx, maxy - miny],
+                "category_id": classes.index(mask["class"]) + 1,
+                "id": annotation_id
+            })
+        return annotations
 
 
 if __name__ == '__main__':
