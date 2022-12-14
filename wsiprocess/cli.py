c5502199a9ca1df5bdc5de7424daa75ac5591f69
tand826 <takumi.ando826@gmail.com>
2022-12-14 14:59:07 +0900

    fix

--- wsiprocess/cli.py
+++ wsiprocess/cli.py
@@ -249,7 +249,7 @@ def main(command=None):
         verbose=args.verbose,
         dryrun=args.dryrun)
     patcher.get_patch_parallel(
-        annotation.classes, max_workems=args.max_workers)
+        annotation.classes, max_workers=args.max_workers)
 
     if args.method == "detection":
         converter = wp.converter(
